1dbf48fae46c3718dba419af6cef43ed8479f43a
Abdelrhman Aiman <153925412+AbdelrhmanAFE@users.noreply.github.com>
2025-08-19 08:39:21 +0300

    Create laravel.sh

---
+++ laravel.sh
@@ -0,0 +1,31 @@
+echo "🔐 Fetching Exchange password once..."
+        gcloud secrets versions access latest --secret=nelc-email-password > /workspace/secret.txt
+        SECRET=$(cat /workspace/secret.txt)
+
+        declare -A LARAVEL_VMS_ZONES=(
+          ["nelc-laravel-stage"]="me-central2-c"
+        )
+        declare -A LARAVEL_ENV_PATHS=(
+          ["nelc-laravel-stage"]="/var/www/html/laravel/multiversity/frontend/pre/.env"
+        )
+        for vm in "${!LARAVEL_VMS_ZONES[@]}"; do
+          zone="${LARAVEL_VMS_ZONES[$vm]}"
+          env_file="${LARAVEL_ENV_PATHS[$vm]:-/var/www/html/.env}"
+          echo "🔧 Updating Laravel on $vm ($zone)... ENV file: $env_file"
+
+          ESCAPED_SECRET=$(printf "%s" "$SECRET" | sed 's/"/\\"/g')
+
+          gcloud compute ssh "$vm" \
+            --zone="$zone" \
+            --tunnel-through-iap \
+            --project="nelc-futurex-prod" \
+            --command="bash -c 'KEY=\"MAIL_PASSWORD\"; \
+              ENV_FILE=\"$env_file\"; \
+              ESCAPED_SECRET=\"$ESCAPED_SECRET\"; \
+              if grep -q \"^\\\$KEY=\" \"\\\$ENV_FILE\"; then \
+                sed -i \"s|^\\\$KEY=.*|\\\$KEY=\\\"\$ESCAPED_SECRET\\\"|\" \"\\\$ENV_FILE\"; \
+              else \
+                echo \"\\\$KEY=\\\"\$ESCAPED_SECRET\\\"\" >> \"\\\$ENV_FILE\"; \
+              fi; \
+              systemctl restart apache2 || true'"
+        done
